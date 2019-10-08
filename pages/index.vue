d2418f35028b4f9cbde5c6adc94f3b54bcc7cf64
pbespalov <pbespalov@rnds.pro>
2019-10-08 11:43:07 +0300

    init

---
+++ pages/index.vue
@@ -0,0 +1,43 @@
+<template lang="pug">
+  .container
+      button(@click="value = !value") QEWQWE
+      transition(name="bounce")
+        p(v-if="value") ASDASD
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      value: true,
+    }
+  },
+}
+</script>
+
+<style lang="scss">
+.container {
+  margin: auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+.bounce-enter-active {
+  animation: bounce-in 0.5s;
+}
+.bounce-leave-active {
+  animation: bounce-in 0.5s reverse;
+}
+@keyframes bounce-in {
+  0% {
+    transform: scale(0);
+  }
+  50% {
+    transform: scale(1.5);
+  }
+  100% {
+    transform: scale(1);
+  }
+}
+</style>
