71affd19c01ef29c34847f593d0fcaa9b3469cad
pbespalov <pbespalov@rnds.pro>
2019-10-11 17:18:39 +0300

    add percy

--- cypress/integration/demo.spec.js
+++ cypress/integration/demo.spec.js
@@ -4,12 +4,14 @@ describe('Some test', () => {
     cy.visit('/')
   })
   it('shuffles fine', () => {
+    cy.percySnapshot('Before shuffle')
     cy.get('#shuffle-list').then((el) => {
       const before = el[0].innerHTML
       cy.get('#shuffle')
         .click()
         .wait(1000)
       cy.get('#shuffle-list').then((el2) => {
+        cy.percySnapshot('After shuffle')
         assert.notEqual(before, el2[0].innerHTML)
       })
     })
